4a8d6c450323bfb86a716944395125744432c7c4
Gkristof96 <kristef.g@gmail.com>
2021-03-21 19:58:15 +0100

    cleaning, and commenting

--- client/src/components/CategoryCard/index.js
+++ client/src/components/CategoryCard/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CategoryCard = ({category, openList}) => {
+const CategoryCard = ({category, openProfessionList}) => {
     return (
-        <div className='category-card' onClick={() => openList(category._id)}>
+        <div className='category-card' onClick={() => openProfessionList(category._id)}>
             <img src={category.image} alt={category.category} />
             <h1>{category.category}</h1>
         </div>
